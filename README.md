de1368ca9fd5d7c0f31f69c238232907ae97d319
SharafatKarim <sharafat2004@gmail.com>
2024-05-25 19:12:13 +0600

    initial deployment and github action testing

---
+++ README.md
@@ -0,0 +1,12 @@
+# Notes
+
+My notes on various topics. It will work automatically, combining all the notes in the `src` directory and generating a TOC (Table of Contents).
+
+## Table of Contents
+
+- [latex](src/latex.md)
+
+## Contribution
+
+Though not anything fancy, but you can feel free to create an issue, pull request, discuss or, clone an get one for yourself!
+Best way to reach me is to knock me on [Telegram](https://t.me/SharafatKarim).
--- builder.py
+++ builder.py
@@ -1,4 +1,36 @@
+#!/usr/bin/env python
+
 # A building block!
 # This script is made to generate a README.md file, combining
 # the contents of the readme directory
 # Generate a TOC for the markdown files of src directory.
+
+import os
+from natsort import natsorted
+
+def generate_readme():
+    # read from readme/head.md
+    with open("readme/head.md", "r") as head:
+        head_md = head.read()
+        with open("README.md", "w") as readme:
+            readme.write(head_md)
+            readme.write("\n")
+    
+    # Generate a TOC for the markdown files of src directory
+    with open("README.md", "a") as readme:
+        readme.write("## Table of Contents\n\n")
+        for root, dirs, files in os.walk("src"):
+            for file in natsorted(files):
+                if file.endswith(".md"):
+                    readme.write(f"- [{file[:-3]}]({os.path.join(root, file)})\n")
+        readme.write("\n")
+    
+    # read from readme/foot.md
+    with open("readme/foot.md", "r") as foot:
+        foot_md = foot.read()
+        with open("README.md", "a") as readme:
+            readme.write(foot_md)
+            readme.write("\n")
+
+if __name__ == "__main__":
+    generate_readme()
