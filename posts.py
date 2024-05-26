e8efb5c3ccef1a860b38b75ed82b2eb54e74486d
SharafatKarim <sharafat2004@gmail.com>
2024-05-26 09:03:30 +0600

    auto posts creation script for hugo branch

---
+++ posts.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# A building block!
+# This script is made to copy markdown files
+# prepand a header like,
+# +++
+# title = "Hello Friend"
+# date = 2021-02-01
+# +++
+# and paste them to the posts directory.
+
+import os
+
+def generate_posts():
+    for root, dirs, files in os.walk("src"):
+        for file in files:
+            if file.endswith(".md"):
+                with open(os.path.join(root, file), "r") as src:
+                    src_md = src.read()
+                    if not os.path.exists("posts"):
+                        os.makedirs("posts")
+                    with open(f"posts/{file}", "w") as post:
+                        post.write("+++\n")
+                        post.write(f"title = \"{file[:-3]}\"\n")
+                        post.write("+++\n")
+                        post.write("\n")
+                        post.write(src_md)
+                        post.write("\n")
+
+if __name__ == "__main__":
+    generate_posts()
